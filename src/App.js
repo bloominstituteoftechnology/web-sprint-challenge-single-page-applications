a6dc7d0076731809d24c306f5dd093aee56b08b9
unknown <admin@social-collab.org>
2020-06-19 13:24:59 -0400

    Setup plus App, Form, and Pizza Edits

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from "react";
+import PizzaMaker from "./PizzaMaker";
+import Pizza from "./Pizza";
+import { Button, Container, FormGroup, Row, Form } from 'reactstrap'
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <Container>
+    <header>
+        <h1>Lambda Eats</h1>
+        <nav>
+          <Form>
+          <Row>
+        <FormGroup>
+          <Button className="col-6" onClick="goHome()">Home</Button> Simple Redirect to Homepage
+          <Button className="col-6" onClick="pizzaMaker()">Build a Pizza</Button>
+        </FormGroup>
+          </Row>
+          </Form>
+        </nav>
+      </header>
+    <PizzaMaker /> { /* Return this only if Build a Pizza Button was clicked */ }
+    {/* <HomePage /> Return this only when Build a Pizza is not open */}
+    <Pizza />
+    </Container>
   );
 };
 export default App;
