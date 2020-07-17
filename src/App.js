cb4449dd5d3ffe52cfcfa32612d6522a2b295b0f
noah niesen <noah@noahs-MacBook-Pro.local>
2020-07-17 18:44:27 -0500

    installed Router, created a components folder, added Home.js to components folder, wrapped App in Router in index.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { Route, Link, Switch } from 'react-router-dom';
+import Home from './components/Home';
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div className='app'>
+      <nav>
+        <h1>Lambda Eats</h1>
+        <div className='nav-links'>
+          <Link to='/'>Home</Link>
+          <Link to='/help'>Help</Link>
+        </div>
+      </nav>
+      <Switch>
+        <Route path='/' component={Home} />
+      </Switch>
+    </div>
   );
 };
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h2>Your favorite food, delivered while coding</h2>
+      <button>Pizza?</button>
+    </div>
+  );
+}
+
+export default Home;
