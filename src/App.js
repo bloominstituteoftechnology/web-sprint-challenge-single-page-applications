fe1879e515174cacca3e92b6ae583974a239be60
Terry Edwards <terryedwardsjr113@gmail.com>
2020-07-18 12:25:02 -0400

    interview questions

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ const App = () => {
   return (
     <>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <p>
+        Computers make pizza perfect everytime! 
+      </p>
     </>
   );
 };
