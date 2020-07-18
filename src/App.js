2b8d39b8c6e75ba981fd2606e7212d816a478ec4
noahbibb21 <noahbibb90@gmail.com>
2020-07-17 20:03:00 -0400

    messed but and forked wrong project but getting done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,43 @@
-import React from "react";
+import React, {useState}from "react";
+// import Form from './components/Form'
+// import Home from './components/Home'
+// import formSchema from './validation/formSchema'
+// import * as yup from 'yup'
+// import axios from 'axios'
+// import {Route, Switch, Link} from 'react-router-dom'
+
+
+const initialFormValues = {
+  first_name: '',
+  size: '',
+  toppings:{
+    pepperoni: false,
+    blackOlives: false,
+    sausage: false,
+    greenChile: false,
+  },
+  substitute: false,
+
+  instructions:'',
+}
+
+const initialFormErrors = {
+  first_name:'',
+  size: '',
+  instructions: '',
+}
+
+const initialDisabled = true
+
+
 
 const App = () => {
+
+  const [orders, setOrders] = useState([])
+  const [formValues, setFormValues] = useState(initialFormValues)
+  const [formErrors, setFormErrors] = useState(initialFormErrors)
+  const [disabled, setDisabled] = useState(initialDisabled)
+
   return (
     <>
       <h1>Lambda Eats</h1>
