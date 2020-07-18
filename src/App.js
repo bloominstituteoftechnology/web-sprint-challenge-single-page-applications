b4d5099548b3aeacf6e40937e972a0f1f2013f81
Donavan Terranova <dterran2@outlook.com>
2020-07-17 19:07:51 -0600

    working on router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
+import {Route, Router} from 'react-router-dom';
+import Home from './Components/Home';
+import Order from './Components/Order';
+
+
 
 const App = () => {
   return (
     <>
       <h1>Lambda Eats</h1>
+      <Route exact path='/' component={Home}/>
+      <Route path='./order' component={Order}/>
       <p>You can remove this code and create your own header</p>
     </>
   );
