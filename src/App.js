5b08f5ebb381abce42c0c6bcef20f430eda8e8d8
Terry Edwards <terryedwardsjr113@gmail.com>
2020-07-18 13:49:13 -0400

    working on form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
+import Form from "./Form";
 
 const App = () => {
   return (
-    <>
+    <div>
       <h1>Lambda Eats</h1>
       <p>
         Computers make pizza perfect everytime! 
       </p>
-    </>
+      <Form/>
+    </div>
+    
   );
 };
 export default App;
