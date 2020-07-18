22026bb3083ab414952070a8924217a7e3f9ac20
Alex Whitt <whitta14@gmail.com>
2020-07-18 14:52:14 -0400

    cypress

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../components/home.css';
 import { Route, Link} from 'react-router-dom';
+import './home.css';
 const HomePage = (props) => {
     
     return(
         <div >
+            <h1> Make Your own Pizza Special</h1>
         </div>
 
     );
