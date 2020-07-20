261230e10574aa920da4879068f87954eec1f056
Damaris Garcia <damaris-garcia@lambastudents.com>
2020-07-19 23:06:31 -0400

    Form Outline Complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, {useState} from "react";
-import Homepage from "./Homepage"
+//import Homepage from "./Homepage"
+import Form from "./Form"
 
 const App = () => {
   const [homePage, setHomePage] = useState([])
   return (
-   <Homepage homePage={homePage} setHomePage={setHomePage} />
+   <Form homePage={homePage} setHomePage={setHomePage} />
   );
 };
 export default App;
