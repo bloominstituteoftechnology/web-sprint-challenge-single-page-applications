4866f2b66a5ce8a029b57dd962ae733be31def2e
matieuruth <matieu.ruth@gmail.com>
2020-07-19 21:18:40 -0400

    linked homepage to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React from 'react';
+import Home from './components/Home'
 
 const App = () => {
   return (
     <>
       <h1>Lambda Eats</h1>
-      
+      <Home/>
     </>
   );
 };
