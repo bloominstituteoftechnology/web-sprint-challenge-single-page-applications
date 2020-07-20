c36d00985503102be4516f5fc519c5171b16fd87
matieuruth <matieu.ruth@gmail.com>
2020-07-19 21:49:16 -0400

    pizza toppings

--- src/components/Pizza.js
+++ src/components/Pizza.js
@@ -1,11 +1,58 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
-
 
 function Pizza() {
     return (
         <>
-            <p>Pizza Comp</p>
+            <form>
+                <label htmlFor='firstName'>First Name
+                    <input 
+                    id='firstName'
+                    minLength='2'
+                    maxLength='20'
+                    placeholder='John'
+                    name='name'
+                    type='text'>
+                    </input>
+                </label><br/>
+                <label htmlFor='lastName'>Last Name
+                    <input 
+                    id='lastName'
+                    minLength='2'
+                    maxLength='20'
+                    placeholder='Smith'
+                    name='name'
+                    type='text'>
+                    </input>
+                </label><br/>
+
+                <label htmlFor='pizzaSize'>Size</label>
+                <select id='pizzaSize'>
+                    <option value='1'>Small (8")</option>
+                    <option value='2'>Medium (12")</option>
+                    <option value='3'>Large (14")</option>
+                    <option value='4'>Extra-Large (18")</option>
+                </select>
+                
+                <input type='checkbox' id='pizzaToppings'/>
+                <label htmlFor='pizzaToppings'>Pepperoni</label>
+
+                <input type='checkbox' id='pizzaToppings'/>
+                <label htmlFor='pizzaToppings'>Cheese</label>
+                
+                <input type='checkbox' id='pizzaToppings'/>
+                <label htmlFor='pizzaToppings'>Pineapple</label>
+                
+                <input type='checkbox' id='pizzaToppings'/>
+                <label htmlFor='pizzaToppings'>Sausage</label>
+                
+                <input type='checkbox' id='pizzaToppings'/>
+                <label htmlFor='pizzaToppings'>Bacon</label>
+                
+                <input type='checkbox' id='pizzaToppings'/>
+                <label htmlFor='pizzaToppings'>Chicken</label>
+
+
+            </form>
         </>
     )
 }
