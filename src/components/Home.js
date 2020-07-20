6eb8fdce7516448e2bcfd3ce8c2db71d00b9eeca
matieuruth <matieu.ruth@gmail.com>
2020-07-19 21:17:10 -0400

    home page comp created

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Route, Link } from 'react-router-dom';
+
+function Home() {
+    return (
+        <div>
+            <p>Hello</p>
+            <p>Hello</p>
+        </div>
+    )
+}
+
+export default Home;
