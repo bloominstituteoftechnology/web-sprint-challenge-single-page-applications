a2370c1d1aa7c9c5ba279e5aa5e6be9e074257e6
karmaeiic <46303520+karmaeiic@users.noreply.github.com>
2020-07-20 17:51:15 -0700

    updated form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -16,4 +16,100 @@ const Form = (props) => {
         instructions:"",
     };
 
+    const [formState, setFormState] = useState(defaultState);
+    const [errors, setErrors] = useState({ ...defaultState });
+    const [buttonDisabled, setButtonDisabled] = useState(true);
+
+    return (
+        <form onSubmit={formSubmit} className="formDiv">
+            <h3>Build Your Own Pizza:</h3>
+            <label htmlForm="name">
+                <input
+                    id="name"
+                    type="text"
+                    name="name"
+                    placeholder="Please Enter Name Here"
+                    onChange={inputChange}
+                    value={formState.name}
+                    errors={errors}
+                />
+            </label>
+            <br />
+            <label htmlFor="pizzaSize">
+                Choice Of Size
+                <select name="pizzaSize" onChange={inputChange}>
+                    <option value="personal">Personal</option>
+                    <option value="small">Small</option>
+                    <option value="medium">Medium</option>
+                    <option value="large">Large</option>
+                </select>
+            </label>
+
+            <label htmlForm="pizzaSauce">
+                Choice of Sauce
+                <select name="pizzaSauce" onChange={inputChange}>
+                    <option value="original">Original Red</option>
+                    <option value="bbq">BBQ Sauce</option>
+                    <option value="garlic">Garlic Ranch</option>
+                    <option value="alfredo">Spinach Alfredo</option>
+                </select>
+            </label>
+
+
+            <div className="toppingDiv">
+                <h3>Add Toppings</h3>
+                <label htmlForm="pepperoni">
+                Pepperoni
+                <input onChange={inputChange} 
+                    name="pepperoni"
+                    type="checkbox"
+                    value="pepperoni"
+                />
+            </label>
+            <label htmlForm="sausage">
+                sausage
+                <input onChange={inputChange} 
+                    name="sausage"
+                    type="checkbox"
+                    value="sausage"
+                />
+            </label>
+            <label htmlFor="onions">
+                Onions
+                    <input onChange={inputChange} 
+                    name="onions" 
+                    value="onions" 
+                    type="checkbox"/>
+            </label>
+            <label htmlFor="olives">
+                Olives
+                    <input onChange={inputChange} 
+                    name="olives" 
+                    value="olives" 
+                    type="checkbox"/>
+            </label>
     
+            
+            </div>
+
+            <label htmlFor="instructions">
+                Special Instructions
+                <input 
+                    id="instructions"
+                    type="text"
+                    name="instructions"
+                    placeholder="type here"
+                    onChange={inputChange}
+                    value={formState.instructions}
+                />
+            </label>
+            <br />
+            <button disabled={buttonDisabled}>Submit</button>
+
+        </form>
+
+    )
+
+}
+
+export default Form
