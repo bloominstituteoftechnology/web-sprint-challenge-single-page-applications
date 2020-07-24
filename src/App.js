f22db9b449c43c97adafb346865e939f1dfa5f91
unknown <olem2233@gmail.com>
2020-07-24 10:42:22 -0700

    not finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { Switch, Link, Route } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 const App = () => {
   return (
-    <>
+    <Router>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+      <div className='navLinks'>
+        <div className='home'>
+        <Link to='/'>Home</Link>
+        </div>
+        <div className='help'>
+        <Link to='/help'>Help</Link>
+        </div>
+        </div>
+        <div className='imgPizza'>
+    <img src= ../public/Wireframes/pizza.jpg>
+  </div>
+    </Router>
   );
 };
 export default App;
