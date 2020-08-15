fa83297c49fea6348c7c19420c580fe4c77a5b77
Aaron Flowers <flowersa29@gmail.com>
2020-08-15 13:33:19 -0500

    8/15a

--- src/index.js
+++ src/index.js
@@ -2,5 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import Home From "./Home"
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(<App />, document.getElementById("root"));
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+    <Router>
+        <App />
+    
+    </Router>,
+    rootElement
+
+);
