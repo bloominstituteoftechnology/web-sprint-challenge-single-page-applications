c67d6531c27682ff8ab46207d81ef0bc8d396e98
kato hawkins <kristhahawkins@icloud.com>
2020-08-15 01:34:19 -0400

    data made for app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
+import data from "./data"
+import "./App.css";
+
 
 const App = () => {
   return (
     <>
       <h1>Lambda Eats</h1>
+      <img src="https://img.huffingtonpost.com/asset/5aa8034f1e00008e0b7ae8a7.jpeg?cache=UKM6vN10o1&ops=scalefit_720_noupscale"></img>
       <p>You can remove this code and create your own header</p>
     </>
   );
---
+++ src/Components/HomeCard.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import{
+    Card,
+    CardBody, 
+    
+}from "reactstrap";
+
+const HomeCard =({ resturant }) => {
+    return (
+        <Col xs="6" md="4" xl="3">
+
+        <Card>
+        
+        </Card>
+        </Col>
+    )
+}
