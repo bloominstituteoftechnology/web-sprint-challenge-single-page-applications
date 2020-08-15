b0c65b1d4deed77242e58ba96f87553952bfaee0
Jeanellle96 <jeanellepazos@gmail.com>
2020-08-14 20:11:20 -0700

    mvp

--- src/App.js
+++ src/App.js
@@ -1,6 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
+import {Link,Route} from "react-router-dom";
+import PizzaCard from "./PizzaCard";
+import Form from "./Form"
+import * as yup from "yup";
+
+
+const fromIdValue = {
+  name: "",
+  size: "",
+  pepporoni: "",
+  hawaiian: false,
+  bbq: false,
+  chicken: false,
+  textarea: "",
+};
+
+const Formerror ={
+  name: "",
+  size: "",
+};
+
+const pizzaValue =[];
 
 const App = () => {
+
+  const [pizza, setPizza] = useState(pizzaValue);
+  const [ formErrors, setFormErrors] = useState(Formerror);
+  const [formValue, setForValue] = useState(fromIdValue);
+
+  const pizzaOnChange = (name, value) =>{
+    setForValue({...formValue, [name]: value });
+  };
+
+const pizzaSubmit = () =>{
+  setPizza([...pizza, formValue]);
+  setForValue(fromIdValue);
+};
+
+
+
   return (
     <>
       <h1>Lambda Eats</h1>
