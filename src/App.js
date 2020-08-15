e309be4d66df9380ecbaadc9fec91b6adb1ea990
Sharon Mosher <sharon.a.mosher@gmail.com>
2020-08-14 23:52:15 -0400

    basic form and homepage done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import { Route, Link, Switch } from 'react-router-dom';
-import Home from './components/Home';
+import Pizza from './components/Pizza'
+import Nav from './components/Nav'
+import { BrowserRouter as Router } from 'react-router-dom'
+import Form from './components/Form';
 
 const App = () => {
+  const [order, setOrder] = useState([]);
   return (
-    <div className='app'>
-      <div className='nav-links'>
-      <nav>
-        <Link to='/'>Home</Link>
-        <Link to='/help'>Help</Link>
-     </nav> 
-     </div>
-
-      <Switch>
-        <Route path='/' component={Home} />
-      </Switch>
-    </div>
+    <Router>
+      <Nav />
+      <div>
+        <Route exact path="/"><h1>Lambda Eats</h1></Route>
+        <Route path="/components/Form"><Form order={order} setOrder={setOrder} /></Route>
+        <Route path="/components/Pizza"><Pizza order={order} /></Route>
+      </div>
+    </Router>
   );
 };
+
 export default App;
