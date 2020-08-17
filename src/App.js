f3dfac2b4f714b6739eee2b6bf095dc1aba29e93
Ryan Clark <rbclark244@gmail.com>
2020-08-16 23:24:15 -0400

    routing is killing me. need to figure out how to link form to how page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { Route } from "react-router-dom";
+import Form from "./Form";
 
 const App = () => {
   return (
     <div>
       <h1>Lambda Eats</h1>
+      <Route path="/Form.js" component={Form} />
       <p>You can remove this code and create your own header</p>
     </div>
   );
