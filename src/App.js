ba324a302da7e95b79801cb1636d011cc7206c70
Ryan Clark <rbclark244@gmail.com>
2020-08-16 21:30:53 -0400

    setup complete

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const App = () => {
   return (
-    <>
+    <div>
       <h1>Lambda Eats</h1>
       <p>You can remove this code and create your own header</p>
-    </>
+    </div>
   );
 };
 export default App;
