82a3360b04d2edd9ac0075df67eafe894e0b36d3
Aaron Flowers <flowersa29@gmail.com>
2020-08-17 20:10:07 -0500

    Aaron-Flowers

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,82 @@
 import React, { useState } from "react";
 
 const Form = props =>{
+
+    const [formState, setFormState] = useState({
+        name:""
+    });
+
+    const formSubmit = e => {
+        e.preventDefault();
+        console.log("form submitted");
+    };
+
+    const inputChange = e => {
+        console.log("input changed", e.target.value);
+        setFormState({name: e.target.value});
+    };
+
+
+
+
+
+
+
+
     return(
 
-     <Form>
-        <input type="text" name="email" placeholder="please place here"/>
-     
-     </Form>
+
+<form onSubmit={formSubmit}>
+
+    <label htmlFor="name">
+        name
+        <input 
+            id="name"
+            type="text"
+            name="name"
+            value={formState.name}
+            onChange={inputChange}
+            />
+    </label>
+
+
+    <label htmlFor="positions">
+        What type of crust would you like?
+        <select id="positions" name="positions">
+			<option>--Please choose an option--</option>
+            <option>Deep Dish</option>
+            <option>Thin Crust</option>
+            <option>Cheesy Crust</option>
+        </select>
+    </label>
+
+    <label htmlFor="sauces" className="sauces" >
+        <input type="checkbox" name="sauces" checked={true}/>
+        <input type="checkbox" name="sauces" checked={true}/>
+        <input type="checkbox" name="sauces" checked={true}/>
+        <input type="checkbox" name="sauces" checked={true}/>
+        <input type="checkbox" name="sauces" checked={true}/> 
+        <input type="checkbox" name="sauces" checked={true}/>
+    
+    
+    </label>
+
+    <label htmlFor="whatelse">
+        What else would you like
+        <textarea id="whatelse" name="whatelse"/>   
+    </label>
+
+  
+    
+
+
+
+
+
+
+
+</form>                  
+    
     );
 };
 export default Form;
