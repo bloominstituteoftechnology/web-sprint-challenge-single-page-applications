548a72a68dcdeed3bf5709cee4696f465388c7f6
donaldlambda <donaldfaulknor25@gmail.com>
2020-08-18 21:06:47 -0400

    Deleted unnecessary comment

--- src/validation/formSchema.js
+++ src/validation/formSchema.js
@@ -11,21 +11,6 @@ const formSchema = Yup.object().shape({
         .oneOf(['small', 'medium', 'large'], "Please select a size")
         .required("Must choose a size."),
 
-    // sauce: Yup
-    //     .mixed()
-    //     .oneOf([
-    //         'pesto' === true,
-    //         'red' === true,
-    //         'white' === true,
-    //         'garlic' === true,
-    //         'spicy' === true,
-    //         'bbq' === true,
-    //     ])
-    //     .required("Please select at least one sauce."),
-    // cheese: Yup
-    //     .array()
-    //     .min([1], "Please select at least one cheese.")
-    //     .required("Please select at least one cheese."),
     instructions: Yup
         .string()
 
