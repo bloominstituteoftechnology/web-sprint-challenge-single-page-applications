d51e7ec55c2d33f95cecbdb6126620f1f318e5ae
gitnubl33t <jessicad8695@gmail.com>
2020-09-12 14:43:50 -0700

    created main image and text for /home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,8 +4,10 @@ import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <div className="home-wrapper">
-      <h1>Your favorite food, delivered while coding</h1>
-      <button className="pizza-form">Pizza?</button>
+      <div className="pizza-banner-content">
+        <h1>Your favorite food, delivered while coding</h1>
+        <button className="pizza-form">Pizza?</button>
+      </div>
     </div>
   );
 }
