3d9fa3d8393e318754e53351ba7bac328d9374e5
gitnubl33t <jessicad8695@gmail.com>
2020-09-12 12:59:06 -0700

    setup Links for Home and Help pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
 import "./App.css";
+import { Link } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="header">
       <h1>Lambda Eats</h1>
       <div className="nav-buttons">
-        <button>Home</button>
-        <button>Help</button>
+        <button>
+          <Link to="/">Home</Link>
+        </button>
+        <button>
+          <Link to="/help">Help</Link>
+        </button>
       </div>
     </div>
   );
