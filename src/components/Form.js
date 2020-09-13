d5a2d3e549726d17aa944c9c332f715b396162fb
unknown <ngriffith29@student.egcc.edu>
2020-09-13 19:11:28 -0400

    added in state to form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 function Form() {
+    const [pizzaInfo, setPizzaInfo] = useState({
+        name: "",
+        size: "",
+        pep: "",
+        sausage: "",
+        pickles:  "",
+        onions: "",
+        specialInstructions: ""
+      });
     return (
         <div>
             
