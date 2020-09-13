45dacf4493d1f2c745b9922e033f7d7a3318ffd8
Damaris Garcia <damaris-garcia@lambastudents.com>
2020-09-13 19:09:20 -0400

    Created a form and homepage.js

---
+++ src/Homepage.js
@@ -0,0 +1,6 @@
+import React, { useState } from "react";
+
+
+const Form = props => {
+    const [pizzaOrder, setPizzaOrder] = useState({name: '', pizzaSize: '', toppingOne: false, toppingTwo:false, toppingThree:false, toppingFour:false, specialRequest: '' })
+}
