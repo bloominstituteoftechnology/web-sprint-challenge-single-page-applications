e98dbc9d3d8a1a0bfa0b3e96b357bb6dfebca608
unknown <ngriffith29@student.egcc.edu>
2020-09-13 19:06:43 -0400

    up and running

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
-
+import Homepage from './components/homepage'
+import Form from './components/Form'
 const App = () => {
   return (
     <>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      
     </>
   );
 };
