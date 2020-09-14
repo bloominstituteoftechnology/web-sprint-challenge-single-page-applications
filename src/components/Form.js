1fb606c4dde80d7eab461b213eebed7ec667f8ac
unknown <ngriffith29@student.egcc.edu>
2020-09-13 20:10:46 -0400

    bringing the form together

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,26 @@
 import React, {useState, useEffect} from 'react'
+import * as yup from "yup";
+import axios from "axios";
+
+const formSchema = yup.object().shape({
+    name: yup.string()
+    .min(2, "Name must be at least two characters")
+    .required("Name is Required"),
+    size: yup.string(),
+    pep: yup.string(),
+    sausage: yup.string(),
+    pickles:  yup.string(),
+    onions: yup.string(),
+    specialInstructions: yup.string()
+  });
+
+
+
 
 function Form() {
+   
+
+
     const [pizzaInfo, setPizzaInfo] = useState({
         name: "",
         size: "",
@@ -12,7 +32,45 @@ function Form() {
       });
 
 
-      const changeHandler = (e) => {
+      const [errorState, setErrorState] = useState({
+        name: "",
+        size: ""
+      });
+
+
+
+      const validate = (e) => {
+        let value =
+          e.target.type === "checkbox" ? e.target.checked : e.target.value;
+        yup
+          .reach(formSchema, e.target.name)
+          .validate(value)
+          .then((valid) => {
+            setErrorState({
+              ...errorState,
+              [e.target.name]: ""
+            });
+          })
+          .catch((err) => {
+            setErrorState({
+              ...errorState,
+              [e.target.name]: err.errors[0]
+            });
+          });
+      };
+
+
+
+      const [buttonDisabled, setButtonDisabled] = useState(true);
+
+      useEffect(() => {
+        formSchema.isValid(pizzaInfo).then((valid) => {
+          setButtonDisabled(!valid);
+        });
+      }, [pizzaInfo]);
+
+
+    const changeHandler = (e) => {
         e.persist();
         validate(e);
     
@@ -23,27 +81,45 @@ function Form() {
      
     }
 
+const subForm = (e) => {
+    e.preventDefault()
+    axios
+  .post("https://reqres.in/api/users", pizzaInfo)
+  .then(res => {
+    console.log(res.data); // Data was created successfully and logs to console
+  })
+  .catch(err => {
+    console.log(err); // There was an error creating the data and logs to console
+  });
+
+    setPizzaInfo({
+        name: "",
+        size: "",
+        pep: "",
+        sausage: "",
+        pickles:  "",
+        onions: "",
+        specialInstructions: ""
+    })
+}
 
 
     return (
         <div>
-        <form onSubmit={subForm}>
+           <form onSubmit={subForm}>
 
 
-     <label htmlFor="name">
-         Type your name
-     <input type="text"
-       name="name"
-       id="name"
-       value={pizzaInfo.name}
-       onChange={changeHandler} />
+        <label htmlFor="name">
+            Type your name
+        <input type="text"
+          name="name"
+          id="name"
+          value={pizzaInfo.name}
+          onChange={changeHandler} />
 
-     </label>
-     <label htmlFor="Sizes">
-     {errorState.name.length > 0 ? (<p className="error">{errorState.name}</p>) : null}
-
-
-
+        </label>
+        <label htmlFor="Sizes">
+        {errorState.name.length > 0 ? (<p className="error">{errorState.name}</p>) : null}
 
 
 
@@ -53,90 +129,93 @@ function Form() {
 
 
 
-     Pick your size
-     <select
-       value={pizzaInfo.size}
-       name="size"
-       id="Sizes"
-       onChange={changeHandler}
-       
-     >
-     <option value="Pick a size">Pick A Size</option>
-       <option value="Small">Small</option>
-       <option value="Medium">Medium</option>
-       <option value="Large">Large</option>
-       
-     </select>
-
-
-     </label>
 
 
 
+        Pick your size
+        <select
+          value={pizzaInfo.size}
+          name="size"
+          id="Sizes"
+          onChange={changeHandler}
+          
+        >
+        <option value="Pick a size">Pick A Size</option>
+          <option value="Small">Small</option>
+          <option value="Medium">Medium</option>
+          <option value="Large">Large</option>
+          
+        </select>
 
 
-     <label htmlFor="pep">
-     <input
-       type="checkbox"
-       id="pep"
-       name="pep"
-       checked={pizzaInfo.pep}
-       onChange={changeHandler}
-     />
-     Pepporoni
-  
-   </label>
+        </label>
 
-   <label htmlFor="sausage">
-     <input
-       type="checkbox"
-       id="sausage"
-       name="sausage"
-       checked={pizzaInfo.sausage}
-       onChange={changeHandler}
-     />
-     Sausage
-  
-   </label>
-   <label htmlFor="pickles">
-     <input
-       type="checkbox"
-       id="pickles"
-       name="pickles"
-       checked={pizzaInfo.pickles}
-       onChange={changeHandler}
-     />
-     Pickles
 
-  
-   </label>
-<label htmlFor="onion">
-     <input
-       type="checkbox"
-       id="onion"
-       name="onions"
-       checked={pizzaInfo.onions}
-       onChange={changeHandler}
-     />
-     onions
-  
-   </label>
 
-   <label htmlFor="instructions">
 
-     <input type="text"
-       name="specialInstructions"
-       id="instructions"
-       value={pizzaInfo.specialInstructions}
-       onChange={changeHandler}
+
+        <label htmlFor="pep">
+        <input
+          type="checkbox"
+          id="pep"
+          name="pep"
+          checked={pizzaInfo.pep}
+          onChange={changeHandler}
+        />
+        Pepporoni
+     
+      </label>
+
+      <label htmlFor="sausage">
+        <input
+          type="checkbox"
+          id="sausage"
+          name="sausage"
+          checked={pizzaInfo.sausage}
+          onChange={changeHandler}
+        />
+        Sausage
+     
+      </label>
+      <label htmlFor="pickles">
+        <input
+          type="checkbox"
+          id="pickles"
+          name="pickles"
+          checked={pizzaInfo.pickles}
+          onChange={changeHandler}
+        />
+        Pickles
+
+     
+      </label>
+ <label htmlFor="onion">
+        <input
+          type="checkbox"
+          id="onion"
+          name="onions"
+          checked={pizzaInfo.onions}
+          onChange={changeHandler}
+        />
+        onions
+     
+      </label>
+
+      <label htmlFor="instructions">
    
-     />
-     Special Instructions
-     </label>
+        <input type="text"
+          name="specialInstructions"
+          id="instructions"
+          value={pizzaInfo.specialInstructions}
+          onChange={changeHandler}
+      
+        />
+        Special Instructions
+        </label>
 
-     <button disabled={buttonDisabled} type="submit">Add to Order</button>
-        </form>
-     </div>
+        <button disabled={buttonDisabled} type="submit">Add to Order</button>
+           </form>
+        </div>
     )
 }
 
