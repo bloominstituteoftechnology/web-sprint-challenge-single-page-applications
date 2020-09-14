29d8663f385df8d61ca4a0cde606d70af7265f15
Damaris Garcia <damaris-garcia@lambastudents.com>
2020-09-13 20:32:08 -0400

    Styling on homepage continues

--- src/Home.js
+++ src/Home.js
@@ -13,13 +13,14 @@ function Home() {
     };
 
     return (
-        <div className="home-wrapper">
+        <div className="home-wrapper" style={{display: "flex", flexFlow: "column"}}>
             <img
+                style={{marginLeft: "5%", marginRight: "5%", width: "90%"}}
                 className="home-image"
                 src={Pizza}
                 alt="margreita pie"
             />
-            <button className="md-button order-button" onclick={navToOrder}>
+            <button className="md-button order-button" onclick={navToOrder} style={{marginTop: "2%", width: "20%", marginLeft: "37%"}}>
                 Click To Order Now!
             </button>
         </div>
