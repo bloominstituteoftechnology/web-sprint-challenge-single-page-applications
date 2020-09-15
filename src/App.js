fc3228fa3e4b94c91d9b37ace5297b0c6dc3db81
Damaris Garcia <damaris-garcia@lambastudents.com>
2020-09-14 20:54:38 -0400

    Adding my routes and links, installed all dependencies

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from "react";
+import Home from "./Home"
+import Pizza from "./Pizza"
+import { Route, Link } from "react-router-dom"
 
 const App = () => {
   return (
     <>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <Link to="/">Home</Link>
+      <Link to="/Pizza">Order Now</Link>
+
+
+
+
+
+      <Route exact path="/">
+        <Home />
+      </Route>
+
+      <Route path="/Pizza">
+        <Pizza />
+      </Route>
+      
     </>
   );
 };
---
+++ src/Pizza.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Pizza = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Pizza;
