88f0d8b694fe898b3951ad00d061d9476e60afbc
Melissa Tanksley <melissa.tanksley@gmail.com>
2020-09-18 08:42:04 -0700

    Cart completed; styled-components downloaded and created

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import { BrowserRouter as Router } from 'react-router-dom'
 import App from "./App";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+
+ReactDOM.render(<Router><App /></Router>, document.getElementById("root"));
