72d0a16c485ae2348d74d050c24e230071d27eb5
Melissa Tanksley <melissa.tanksley@gmail.com>
2020-09-18 10:30:31 -0700

    updated testing

--- cypress/integration/form.spec.js
+++ cypress/integration/form.spec.js
@@ -1,6 +1,6 @@
 describe('Goes to localhost', () => {
     beforeEach(() => {
-      cy.visit('http://localhost:3000/')
+      cy.visit('http://localhost:3001/')
     })
   
     it('does stuff', () => {
