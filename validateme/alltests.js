eb3ff824d6f8075e39b0b747dce03f6dabf964f2
Simone Ballard <simone.a.ballard@gmail.com>
2020-09-18 11:54:29 -0600

    tests added

--- validateme/alltests.js
+++ validateme/alltests.js
@@ -2,6 +2,10 @@ describe("Pizza app", () => {
 
 const textInput = () => cy.get("input[name=text]");
 const instructionsInput = () => cy.get("input[name=instructions")
+const pepperoniInput = () => cy.get("input[name=pepperoni")
+const olivesInput = () => cy.get("input[name=olives")
+const tomatoesInput = () => cy.get("input[name=tomatoes")
+const spinachInput = () => cy.get("input[name=spinach")
 const submitBtn = () => cy.get(`button[id="submitBtn"]`);
 
 it("can type in the inputs", () => {
@@ -17,7 +21,19 @@ it("can type in the inputs", () => {
       .should("have.value", "I want my pizza badly cooked");
   });
 
-  it("can submit a new quote", () => {
+
+it("multiple checkboxes are showing", () => {
+        pepperoniInput().should("exist");
+        olivesInput().should("exist");
+        tomatoesInput().should("exist");
+        spinachInput().should("exist");
+
+    cy.contains("Submit Quote").should("exist");
+    cy.contains(/submit quote/i).should("exist");
+    });
+        
+
+  it("can submit a new text string", () => {
     
     cy.contains(/have fun/).should("not.exist");
     textInput().type("Alice");
