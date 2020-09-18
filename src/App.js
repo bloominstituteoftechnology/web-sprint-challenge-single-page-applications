a1e259537a59a5e901f676ff7f8e980a8391ddf9
Melissa Tanksley <melissa.tanksley@gmail.com>
2020-09-18 08:34:19 -0700

    cypress downloaded; App.js completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import { Route } from "react-router-dom";
+import Nav from "./Components/Nav";
+import OrderForm from "./Components/OrderForm";
+import Cart from "./Components/Cart";
+import Home from "./Components/Home";
 
 const App = () => {
+  const [order, setOrder] = useState([]);
+  console.log("App -> order", order)  
+
   return (
     <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+    <Nav />
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route exact path="/order">
+        <OrderForm order={order} setOrder={setOrder} />
+      </Route>
+      <Route exact path="/cart">
+        <Cart order={order} />
+      </Route>
     </>
   );
-};
+}; 
 export default App;
+
+
+
+/*
+
+  
+  
+
+/*
+  // const tempOrder = [{
+  //   customer: "Tiffany",
+  //   toppingsChecked: [{ name: 'Grilled Chicken', id: 'grilled-chicken', isChecked: false },
+  //   { name: 'Onions', id: 'onions', isChecked: false },
+  //   { name: 'Green Pepper', id: 'green-pepper', isChecked: false }],
+  //   size: "Medium",
+  // }]
+  */
