67399f30a0756cd0df752a9fa884f8b72d5c2158
Melissa Tanksley <melissa.tanksley@gmail.com>
2020-09-18 10:24:45 -0700

    cypress testing

---
+++ cypress/integration/form.spec.js
@@ -0,0 +1,15 @@
+describe('Goes to localhost', () => {
+    beforeEach(() => {
+      cy.visit('http://localhost:3000/')
+    })
+  
+    it('does stuff', () => {
+      cy.get('[data-cy="orderLink"]').click()
+      cy.get('[data-cy="customer"]').type("Tiffany").should("have.value", "Tiffany")
+      cy.get('[data-cy="size"]').select("small").should("have.value", "small")
+      cy.get('[data-cy="pepperoniTest"]').check()
+      cy.get('[data-cy="sausageTest"]').check()
+      cy.get('[data-cy="submit"]').click()
+      cy.get('[data-cy="cartLink"]').click()
+    })
+  }) 
