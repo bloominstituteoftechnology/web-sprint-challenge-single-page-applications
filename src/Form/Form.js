d9be125dd9be17b51ffed59c92e83cfca66aaaef
extrude575757 <nicholas.a.lacapria@gmail.com>
2020-10-10 12:53:32 -0700

    Form parameters on submit

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,13 +1,13 @@
 
 import React from 'react'
-
+import '../App.css';
 
 const Form = () =>{
 
 
     return (
-        <div>
-            <form>
+        <div className="App">
+            <form className="App">
                 <label htmlFor="name">Name</label>
                 <input type="text" name="name" />
                 <label htmlFor="Psize">Size</label>
