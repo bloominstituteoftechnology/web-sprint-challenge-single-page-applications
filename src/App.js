f429fa9b2053859ffb69dce12a3bbd97f0d5fabb
Austin Littlefield <austinlittlefield@MacBook-Pro.local>
2020-10-11 18:20:29 -0700

    Created Pizza Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
+import {Route, Link} from "react-router-dom";
 
 const App = () => {
   return (
-    <>
+    <div>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+      <Link to="/">Home</Link>
+    </div>
   );
 };
 export default App;
