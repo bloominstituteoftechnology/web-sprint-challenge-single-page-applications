a630f3ceb79e1673457bc0599be63e31300dfdaa
Robel Mengistu <62121236+robelv2020@users.noreply.github.com>
2020-10-11 20:13:55 -0600

    Started working on the Home

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import Form from './Components/Form';
+import home from './Components/Home';
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <Router>
+      <div className='App'>
+        <nav className='navbar'>
+          <Link>
+            <button name='home'>Home</button>
+          </Link>
+          <Link to='/form'>Order</Link>
+        </nav>
+      </div>
+      <div className='App-header'>
+        <Switch>
+          <Route exact path='/' component={home} />
+          <Route path='/form' component={Form} />
+          <Route
+            path='/order'
+            render={(props) => {
+              return null;
+            }}
+          />
+        </Switch>
+      </div>
+    </Router>
   );
 };
 export default App;
