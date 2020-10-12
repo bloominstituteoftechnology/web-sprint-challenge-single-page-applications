3fca4059a87f3440a821f9a01f63f037505bd497
Austin Littlefield <austinlittlefield@MacBook-Pro.local>
2020-10-11 18:32:20 -0700

    Pizza Page Links to Home

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import {Route, Link} from "react-router-dom";
+import Pizza from"./components/pizza"
 
 const App = () => {
   return (
     <div>
       <h1>Lambda Eats</h1>
       <Link to="/">Home</Link>
+      <Route exact path="/pizza" component={Pizza}>Home</Route>
+      <Link to="/pizza">Pizza</Link>
     </div>
   );
 };
---
+++ src/components/pizza.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+
+const Pizza = () => {
+    return (
+        <div>
+          <h1> Ordere Here</h1>
+        </div>
+      );
+}
+
+export default Pizza
