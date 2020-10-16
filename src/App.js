802f466bb464d31e470c5786d7d3af8727df383c
MorganWilliamson <mwilliamson7695@gmail.com>
2020-10-16 10:41:44 -0500

    Added import statements, building links in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { Route, Link, Switch } from "react-router-dom";
+import "./App.css" //if there's time for styling
+import Home from "./components/Home";
+import Pizza from "./components/Pizza";
+//schema?
+
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+      <div className="home">
+        <h1>Lambda Eats!</h1>
+          <p>Food for thought!</p>
+        
+        <div className="navlinks">
+          <Link exact to="/">Home</Link>
+          <Link to="/pizza">Order Here!</Link>
+        </div>
+      </div>
   );
 };
 export default App;
