b8fb26ff8c9dcf86b872f871b14cd985f7786d2a
Brian Abeyta-Pratt <babeytapratt@gmail.com>
2020-10-16 12:05:01 -0600

    home page started

--- src/Home.js
+++ src/Home.js
@@ -1,2 +1,14 @@
 import React, { useState, useEffect} from 'react';
 import {useParams, NavLink, Route, useRouthMatch, useHistory } from 'react-router-dom'
+
+export default function Home() {
+
+    return (
+        <header>
+            <h1>Lambda Eats</h1>
+            <nav>
+
+            </nav>
+        </header>
+    )
+}
