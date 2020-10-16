3bd26393f99031040d167ee746cf5dc3a8f2876c
Sam Singer <sam.singet79@gmail.com>
2020-10-16 10:26:08 -0700

    sam-singer

---
+++ src/FormPage/indexStyles.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components'
+
+export const FormWrap = styled.div`
+    max-width: 100%;
+`
+export const FormHead = styled.div`
+    font-size: 3rem;
+`
+export const HeroHead = styled.h1`
+    font-size: 2rem;
+ `  
+ export const HeroImg = styled.img`
+    max-width: 100%;
+ `
+ export const FormInputs = styled.div`
+ max-width: 100%;
+ display: flex;
+ flex-direction: column;
+ `
+ export const FormLabel = styled.label`
+ font-size: 1rem;
+ `
+ export const SizeSelect = styled.select`
+    padding: 1rem;
+ `
+  export const SizeOption = styled.option`
+  padding: 1rem;
+`
+export const SauceCheck = styled.input`
+padding: 1rem`
