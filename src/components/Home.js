2099e8e7dd397db20384cd99d7b6f9a1f1b1e3ab
antoine-bibb <ajbibb0114@gmail.com>
2020-10-23 10:32:20 -0400

    fixed minor bugs

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,31 +1,37 @@
-import React from "react"
-import img from '../Assets/Pizza.jpg'
-import styles from 'styled-components'
+import React from "react";
+import img from "../Assets/Pizza.jpg";
+import styles from "styled-components";
 
 const Home = () => {
-    
- 
-    return(
-        <Styles>
-            <h2>Your favorite food delieverd while coding</h2>
-         <Button>Pizza?</Button>
-         
-         <img className="logo" src={img} alt=""></img>
-         
-
-
-
-           
-        </Styles>
-    )
-}
+  return (
+    <Styles>
+      <div className="bg-img">
+        <div className="head">
+          <h2>Your favorite food delieverd while coding</h2>
+          <Button>Pizza?</Button>
+        </div>
+      </div>
+      {/* <img className="logo" src={img} alt=""></img> */}
+    </Styles>
+  );
+};
 const Button = styles.button`
 width:20%;
+display:flex;
+justify-content:center;
 margin:auto;
-`
+
+padding:1.5%;
+border:2px solid black;
+`;
+
+const Bg = styles.div`
+height:50vh;
+display:flex;
+`;
 
 const Styles = styles.div`
-width:45%;
+width:100%;
 display: flex;
 flex-flow:column;
   justify-content: center;
@@ -33,5 +39,5 @@ flex-flow:column;
   border:2px solid purple;
   
   
-`
-export default Home
+`;
+export default Home;
