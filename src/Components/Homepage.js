9a176c45400340e8b6f4ef217c0a427360961667
Amanda Nelson <amanda.gaylord@gmail.com>
2020-11-08 16:30:30 -0500

    Link to /pizza added to Homepage.js

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Homepage = () => {
   return (
     <>
       <p>Homepage HERE</p>
+      <Link to={"/pizza"}>
+        <p className="orderPizza">Order Pizza</p>
+      </Link>
     </>
   );
 };
