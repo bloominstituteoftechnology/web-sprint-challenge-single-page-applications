4d5d3ad701f113e30f8e0de86ed14a2667d9390a
Amanda Nelson <amanda.gaylord@gmail.com>
2020-11-08 16:17:33 -0500

    React Router Dom added to project.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { Route, Link } from "react-router-dom";
+import Homepage from "./Components/Homepage";
+import Form from "./Components/Form";
 
 const App = () => {
   return (
     <>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      {/*Header*/}
+      {/*Footer*/}
     </>
   );
 };
