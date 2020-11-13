88d586d29b244e3c5cad085fa3311abac616a4d7
Adham AbdelFattah <adham.adel@gmail.com>
2020-11-13 11:52:55 -0600

    created and styled the header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
 import React from "react";
+import styled from 'styled-components';
+import {Button} from 'reactstrap';
 
 const App = () => {
   return (
     <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      {/* Create the header */}
+      <StyledHeader>
+      <header className='site-header'>
+        <h1 className='app-name'>Lambda Eats</h1>
+        <div className='navigation-toggle'>
+          <Button className='navigation-button' color='secondary'> Home </Button>
+          <Button className='navigation-button' color='secondary'> Help </Button>
+        </div>
+      </header>
+      </StyledHeader>
+      
     </>
   );
 };
 export default App;
+
+const StyledHeader = styled.header `
+display: flex;
+flex-flow: row;
+justify-content: center;
+border: 1px solid red;
+
+  .site-header {
+    border: blue 1px dashed;
+    min-width: 90%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+  .app-name {
+    padding-left: 10px;
+  }
+  .navigation-toggle {
+    padding-right: 10px;
+  }
+  .navigation-button {
+  margin: 1px;
+}
+`
