9672e216722d7e0108e692a56fd726ca42de19b2
Jesse Medrano <Jtamedrano@gmail.com>
2020-12-11 18:15:58 -0800

    init app edit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React from "react";
+import React from 'react';
 
 const App = () => {
   return (
     <>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
     </>
   );
 };
