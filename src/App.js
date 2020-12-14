f266911147513b1325fc52aae13fbf67400c95b2
Melissa <wolfaholic@users.noreply.github.com>
2020-12-13 22:10:07 -0800

    progressing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+// import 'src\index.css';
  
 
 const NavDiv = styled.div`
@@ -8,7 +9,7 @@ const NavDiv = styled.div`
     flex-direction: row;
     flex-wrap: nowrap;
     justify-content: space-between;
-    font-family: sans-serif;
+   
     text-align: center;
     border-bottom: solid 5px red;
     margin: 0 auto;
@@ -16,16 +17,20 @@ const NavDiv = styled.div`
 
 const NavHeader = styled.h1`
     align-self: center;
-    color: red;
-    border: solid 2px red;
+    font-family: 'Anton', sans-serif;
+    background: rgb(242,7,7);
+    background: linear-gradient(90deg, rgba(242,7,7,1) 0%, rgba(255,175,0,0.8267682072829132) 100%);
+    color: White;
+    border: solid 3px linear-gradient(90deg, rgba(242,7,7,1) 0%, rgba(255,175,0,0.8267682072829132) 100%);;
     padding: 10px 20px;
+    box-shadow: 5px 5px 5px rgba(43, 4, 2, .5);
 `;
 
 const NavLinks = styled.div`
     align-self: center;
     display: flex;
     font-weight: bold;
-    font-family: sans-serif;
+    font-family: Lato, sans-serif;
     `;
 
 const ButtonLinks = styled(Link)`
@@ -45,10 +50,34 @@ const HeroDiv = styled.div`
     
 `;
 
+const TopCards = styled.div`
+    background-color: red;
+    border-bottom: solid 3px white;
+    
+`;
+const BottomCards = styled.div`
+    background-color: red;
+    border-bottom: solid 3px white;
+    
+`;
+
+const PizzaButton = styled(ButtonLinks)`
+    border-radius: 20%;
+    border: none;
+    box-shadow: 5px 5px 5px rgba(43, 4, 2, .5);
+`;
+
+const MainDiv = styled.div`
+display: block;
+margin: 0;
+padding: 0;
+background: rgb(221,221,221);
+background: linear-gradient(90deg, rgba(221,221,221,1) 0%, rgba(255,119,0,0.8323704481792717) 100%);
+`;
 
 export default function App() {
     return ( 
-       <div>
+       <MainDiv>
             <NavDiv>
                 <NavHeader>Lambda Eats</NavHeader>
                 <NavLinks>
@@ -57,8 +86,42 @@ export default function App() {
                 </NavLinks>
             </NavDiv>
 
-            <HeroDiv></HeroDiv>
-        </div>
+            <HeroDiv>
+              
+                    <h2>Your favorite food while coding</h2>
+                    <img src="Assets/Pizza.jpg" alt="Pizza" />
+                    <PizzaButton to="orderform">Pizza?</PizzaButton>
+                 
+            </HeroDiv>
+
+            <TopCards>
+                    <h2>Best Food Deliver for those late night pushes!</h2>
+                    <div>
+                    <button>McDonalds</button> 
+                    </div>
+                    <div>
+                    <button>Taco Bell</button>    
+                    </div>
+                    <div>
+                    <button>SubWay</button> 
+                    </div>
+
+            </TopCards>
+            <BottomCards>
+                    <h2>Best Food Deliver for those late night pushes!</h2>
+                    <div>
+                    <button>McDonalds</button> 
+                    </div>
+                    <div>
+                    <button>Taco Bell</button>    
+                    </div>
+                    <div>
+                    <button>SubWay</button> 
+                    </div>
+
+            </BottomCards>
+          
+        </MainDiv>
 
      
     );
