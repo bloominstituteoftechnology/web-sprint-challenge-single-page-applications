18c3d427d6f10bb8bae96fa170ced9efc190d67d
Corie Stewart <coriestewart1989>
2020-12-18 10:40:27 -0600

    route paths added to app

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import React from "react";
 import { Header } from './Components/Header'
 import { Form } from './Components/Form'
 import { Home } from './Components/Home'
+import { Route, Switch } from "react-router-dom";
 // import { Route, Switch } from 'react-router-dom'
 
 const App = () => {
   return (
     <>
       <Header/>
-      <Form/>
-      <Home/>
+      <Switch>
+          <Route path='/pizza'>
+            <Form/>
+          </Route>
+          <Route path = '/'>
+            <Home/>
+          </Route>
+      </Switch>
     </>
   );
 };
