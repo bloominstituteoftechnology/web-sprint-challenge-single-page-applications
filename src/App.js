d6df8e9425befa99ceb033a4b19bab5d786f34a9
Anthony Cortese <akcortese@Anthonys-MacBook-Pro.local>
2020-12-18 11:34:30 -0700

    reworking project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Route, Link, Switch } from "react-router-dom"
+import HomePage from "./HomePage";
+import Form from "./Form"
+import Pizza from "./Pizza"
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div className ="App">
+      <nav>
+      <h1 className="store-header"> Lambda Eats</h1>
+      <div className="nav-links">
+      <p>Pizza Shop</p>
+      <Link to="/">Home</Link>
+      
+      </div>
+      </nav>
+    </div>
+//   <Switch>
+//   <Route path={"/items-list/:itemId"}>
+//     <Item items={stock} />
+//   </Route>
+//   <Route path="/items-list">
+//     <ItemsList items={stock} />
+//   </Route>
+//   <Route path="/">
+//     <Home />
+//   </Route>
+// </Switch>
+  
+  
   );
+
 };
 export default App;
