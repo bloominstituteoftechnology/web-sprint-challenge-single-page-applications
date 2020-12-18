b9f9f122ce6bc53b9d7e773964982e1a8520a72b
Matthew Justice <matthewajustice@gmail.com>
2020-12-18 13:48:57 -0500

    on to testing, first commit, wasted time on confirmation page cause I misread the instructions

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './Header';
+import HeroImg from './HeroImg';
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <HeroImg />
+    </div>
+  );
+}
