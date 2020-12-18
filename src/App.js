64707772b295202db4af02bccef95f17df974acc
shawn2776 <=>
2020-12-18 10:44:09 -0800

    not yet completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
+import Navbar from './Navbar';
+import Home from './Home';
+import PizzaForm from './PizzaForm';
+
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div className="App">
+      <Navbar />
+      <Home />
+      <PizzaForm />
+    </div>
   );
 };
 export default App;
---
+++ src/Home.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Container, Row, Col } from 'reactstrap';
+import { Jumbotron, Button } from 'reactstrap';
+
+const Home = () => {
+  return (
+    <Container fluid>
+      <Row>
+        <Col>
+        Some Text
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          Some Text
+        </Col>
+        <Col>
+          Some Text
+        </Col>
+      </Row>
+    </Container>
+  )
+}
+
+export default Home
