45693d3cfa3a6c43006236c1fff8041942e9c777
Tony Miller <70006926+artofmayhem@users.noreply.github.com>
2021-01-24 22:32:04 -1000

    Update tests.spec.js

--- cypress/e2e/tests.spec.js
+++ cypress/e2e/tests.spec.js
@@ -3,34 +3,34 @@
 
 // write tests here
 
-// describe("Pizza App", () => {
-//     beforeEach(() => {
-//       cy.visit("/");
-//     });
-//     it("sanity check", () => {
+describe("Pizza App", () => {
+    beforeEach(() => {
+      cy.visit("/");
+    });
+    it("sanity check", () => {
      
-//       expect(2 + 2).to.equal(4);
-//       expect(2 + 2).not.to.equal(5);
-//     });
+      expect(2 + 2).to.equal(4);
+      expect(2 + 2).not.to.equal(5);
+    });
   
-//     it("sanity check 2", () => {
-//       expect([1, 2, 3].length).to.equal(3);
-//     });
+    it("sanity check 2", () => {
+      expect([1, 2, 3].length).to.equal(3);
+    });
   
-//     const textInput = () => cy.get('[name="text"]');
-//     const sizeInput = () => cy.get('[name="size"]');
-//     const sauceInput = () => cy.get('[name="sauceChoice"]');
-//     const submitButton = () => cy.get('#submit-btn');
+    const textInput = () => cy.get('[name="text"]');
+    const sizeInput = () => cy.get('[name="size"]');
+    const sauceInput = () => cy.get('[name="sauceChoice"]');
+    const submitButton = () => cy.get('#submit-btn');
   
-//     it("input fields rendering", () => {
-//       textInput().should("exist");
-//       sizeInput().should("exist");
-//       sauceInput().should("exist");
-//       submitButton().should("exist");
-//     });
+    it("input fields rendering", () => {
+      textInput().should("exist");
+      sizeInput().should("exist");
+      sauceInput().should("exist");
+      submitButton().should("exist");
+    });
   
     
    
   
-//    });
+   });
   
