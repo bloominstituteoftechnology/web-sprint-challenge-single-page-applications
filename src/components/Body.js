19b70753471f8bbcc1f155a01516a19d814a2814
Saeed Khan <saeedkhan@Saeeds-MacBook-Pro.local>
2021-01-25 18:58:53 -0700

    Header Component

---
+++ src/components/Body.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Body() {
+    
+
+
+    return (
+        <>
+        <div className="body">
+        <body>
+            <h2>Popular Near You</h2>
+        </body>
+        </div>
+        </>
+    )
+}
