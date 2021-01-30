e196cbffcdb9c3306316fda71f27b475d3f2e431
Sarah Mucho <64818980+SJMucho@users.noreply.github.com>
2021-01-30 14:02:28 -0500

    components set up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import PizzaForm from "./components/PizzaForm.js"
+import Home from "./components/Home.js"
 
 const App = () => {
   return (
     <>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+     
     </>
   );
 };
---
+++ src/components/PizzaForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PizzaForm = () => {
+
+  return (
+
+    <div>Order Here:</div>
+
+  )
+}
+
+export default PizzaForm;
