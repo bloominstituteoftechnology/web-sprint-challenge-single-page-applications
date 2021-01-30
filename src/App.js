ffed2ca662c3c4391be53a9e827b452e6f7f033d
Sarah Mucho <64818980+SJMucho@users.noreply.github.com>
2021-01-30 14:56:22 -0500

    pizza component coming along but not rendering

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from "react";
-import PizzaForm from "./components/PizzaForm.js"
-import Home from "./components/Home.js"
+import PizzaForm from "./components/PizzaForm.js";
+import Home from "./components/Home.js";
+import { Route, Switch, Link } from "react-router-dom";
+import { Button } from "reactstrap";
 
 const App = () => {
   return (
     <>
       <h1>Lambda Eats</h1>
-     
+      <div>
+        <Link to="/form">
+          {" "}
+          <Button>Order pizza here</Button>{" "}
+        </Link>
+        <Link to="/">
+          <Button>Home</Button>
+        </Link>
+      </div>
+
+      <Switch>
+        <Route exact path="/" component={Home} />
+        {/* <Route path="form" render={() => <PizzaForm />} />  */}
+        <Route path="form" component={PizzaForm} />
+      </Switch>
     </>
   );
 };
--- src/components/PizzaForm.js
+++ src/components/PizzaForm.js
@@ -1,12 +1,110 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import * as yup from "yup";
+import { Form, Row, Col } from "reactstrap";
 
 const PizzaForm = () => {
+  const [formState, setFormState] = useState({
+    name: "",
+    size: "",
+    pepperoni: "",
+    sausage: "",
+    olives: "",
+    mushrooms: "",
+    instructions: "",
+  });
+
+  const [errors, setErrors] = useState({
+    name: "",
+    size: "",
+    pepperoni: "",
+    sausage: "",
+    olives: "",
+    mushrooms: "",
+    instructions: "",
+  });
+
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+
+  const formSchema = yup.object().shape({
+    name: yup.string().required("name is required"),
+    size: yup.string().oneOf(["small", "medium", "large"]),
+    pepperoni: yup.string().oneOf([true]),
+    sausage: yup.string().oneOf([true]),
+    olives: yup.string().oneOf([true]),
+    mushrooms: yup.string().oneOf([true]),
+    instructions: yup.string(),
+  });
+
+  const validateChange = (e) => {
+    yup
+      .reach(formSchema, e.target.name)
+      .validate(e.target.value)
+      .then((valid) => {
+        setErrors({ ...errors, [e.target.name]: "" });
+      })
+      .catch((err) => {
+        console.log("error", err);
+        setErrors({ ...errors, [e.target.name]: err.errors[0] });
+      });
+  };
+
+  console.log("error state", errors);
+
+  useEffect(() => {
+    formSchema.isValid(formState).then((valid) => {
+      console.log("valid?", valid);
+      setIsButtonDisabled(!valid);
+    });
+  }, [formState]);
+
+  const formSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("https://reqres.in/", formState)
+      .then((res) => {
+        SVGTextPositioningElement(res.data);
+        setFormState({
+          name: "",
+          size: "",
+          pepperoni: "",
+          sausage: "",
+          olives: "",
+          mushrooms: "",
+          instructions: "",
+        });
+      })
+      .catch((err) => console.log(err.response));
+  };
+
+  const onInputChange = (e) => {
+    console.log("input change", e.target.value);
+    e.persist();
+    const newFormData = {
+      ...formState,
+      [e.target.name]:
+        e.target.type === "checkbox" ? e.target.checked : e.target.value,
+    };
+    validateChange(e);
+    setFormState(newFormData);
+  };
 
   return (
-
-    <div>Order Here:</div>
-
-  )
-}
+    <div>
+      <p>Order here:</p>
+      <form onSubmit={formSubmit}>
+        <label htmlFor="name">
+          name
+          <input
+            id="name"
+            name="name"
+            onChange={onInputChange}
+            value={formState.name}
+          />
+        </label>
+      </form>
+    </div>
+  );
+};
 
 export default PizzaForm;
