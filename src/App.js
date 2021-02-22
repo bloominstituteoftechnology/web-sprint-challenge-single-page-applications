b7943b7264c40b02ea361f1e9cab3c4ccc7eeb5c
MaurooRen <maurooren@gmail.com>
2021-02-21 20:20:29 -0500

    created 404 page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
 import { Route, Link, Switch } from 'react-router-dom';
 import Home from "./layout/Home";
+import NewPizza from "./layout/NewPizza";
+import NoMatch from "./layout/NoMatch";
 
 
 const App = () => {
   return (
     <Switch>
-      <Route path="/">
+      <Route path="/new-pizza">
+        <NewPizza />
+      </Route>
+      <Route exact path="/">
         <Home />
+      </Route>
+      <Route path="*">
+        <NoMatch />
       </Route>
     </Switch>
   );
---
+++ src/layout/NewPizza.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewPizza = () => {
+    return (
+        <div>
+            <h1>New Pizza</h1>
+        </div>
+    )
+}
+
+export default NewPizza
