ac72742dc3ebcc6542710c9cb2dfc460c8e5bc17
abemm7787 <mirambeaux7@gmail.com>
2021-04-23 18:55:10 -0500

    everything updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,84 @@
-import React from "react";
+import App from './App.css'
+import React, {useState, useEffect} from "react"
+import {BrowserRouter, Link, Route, Switch} from "react-router-dom";
+import * as Yup from "yup";
+import axios from "axios"
+import Nav from  "./nav"
 
-const App = () => {
+
+
+
+
+
+
+
+const Apps = () => {
+
+// State Hooks
+
+
+
+
+// const users = [
+
+
+
+
+  //JSX
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
-  );
-};
-export default App;
+
+ 
+      <div className="APP">
+       <Nav/> 
+          <header className= "Heads">
+        <Switch>
+                                                   
+        <Route exact path ="/"  component={Home}/>  
+        <Route path= "/shop" component = {Form}/>        
+             </Switch>"
+                                        
+              </header>
+                                    
+                                                        
+              </div>     
+                                                 
+                         );
+                               
+                         };
+                                                    
+
+       
+
+             export default Apps   
+                                                 
+                  
+
+                                                
+
+                             
+
+                         
+                                   
+                             
+         
+         
+         
+                            
+                             
+                              
+ 
+
+
+                               
+                               
+                               
+                               
+                      
+                      
+                                 
+                             
+          
+                                    
+  
+
+
