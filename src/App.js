8bda7d7928e356fa2469731a2e6acad6c981c9d6
ricardo-pena <ricardo.penagarcia06@gmail.com>
2021-04-23 16:50:05 -0400

    Created the Nav Bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from "react";
 import Form from './Form';
+import Home from './Home'
+import { BrowserRouter as Router , Link, Switch,NavLink } from 'react-router-dom';
+
 
 const App = () => {
   return (
     <>
-      <Form/>
+    <header>
+      <nav>
+        <h1>Ricky's Pizzeria</h1>
+        <div className ="navLinks">
+          <Router>
+            <Link to='/'>Home</Link>
+            <Link to ='/order'>Order</Link> 
+
+          </Router>
+
+          
+        </div>
+      </nav>
+
+    </header>
+    <Home/>
+      {/* <Form/> */}
     </>
   );
 };
--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,21 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
 
 
 export default function Home (){
+    const history = useHistory();
+
+    // Functions:
+    const routeToOrder = ()=>{
+        history.push('/order');
+    }
+
     return (
-        <div className ="container">
-            
+        <div className ="homeContainer">
+            {/* Background Img */}
+            <img className ="homeImage" src ="https://pwpizza.com/wp-content/uploads/2019/05/pw_pizza_home_hero-2-1024x682.jpg" alt ="pizza"/>
+            {/* Order Btn */}
+            <button onClick = {routeToOrder} className ="orderBtn">Order Now!</button>
         </div>
     )
 }
