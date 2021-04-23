2326633cf025e3af1676be7d021f0d902e2610a8
ricardo-pena <ricardo.penagarcia06@gmail.com>
2021-04-23 19:57:04 -0400

    Added test

--- src/Schema.js
+++ src/Schema.js
@@ -2,7 +2,7 @@ import * as yup from "yup"
 
 export default yup.object().shape({
     name: yup.string().required("Name is required").min(2, "name must be at least 2 characters"),
-    size: yup.string().required("Size is required"),
+    size: yup.string(),
     pineapple: yup.boolean().oneOf([true,false],""),
     jalapenos: yup.boolean().oneOf([true,false],""),
     mushrooms: yup.boolean().oneOf([true,false],""),
