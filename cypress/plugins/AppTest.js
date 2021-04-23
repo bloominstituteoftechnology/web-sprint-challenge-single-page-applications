4208c59fa8c51b59c1f1bd16c0862ae357dbf393
ricardo-pena <ricardo.penagarcia06@gmail.com>
2021-04-23 19:49:22 -0400

    Started constructing test

--- cypress/plugins/AppTest.js
+++ cypress/plugins/AppTest.js
@@ -4,5 +4,10 @@ describe("Quotes app", () => {
         cy.visit("http://localhost:1234");
       });
     
+    const nameInput = ()=>cy.get('imput[name="name"');
+    const sizeInput = ()=>cy.get('imput[name="size"');
+    const Input = ()=>cy.get('imput[name="name"');
+    const 
+
 
 });
