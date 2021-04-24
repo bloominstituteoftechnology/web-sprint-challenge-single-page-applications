2f35898eca2dfb1817bf2b714579e60b69fb1ec6
abemm7787 <mirambeaux7@gmail.com>
2021-04-24 00:28:35 -0500

    sprint challenge

--- src/App.js
+++ src/App.js
@@ -1,64 +1,48 @@
-import App from './App.css'
-import React, {useState, useEffect} from "react"
-import {BrowserRouter, Link, Route, Switch} from "react-router-dom";
-import * as Yup from "yup";
-import axios from "axios"
-import Nav from  "./nav"
-
-
-
-
-
-
-
-
-const Apps = () => {
-
-// State Hooks
-
-
-
-
-// const users = [
-
-
-
-
-  //JSX
-  return (
-
- 
-      <div className="APP">
-       <Nav/> 
-          <header className= "Heads">
-        <Switch>
-                                                   
-        <Route exact path ="/"  component={Home}/>  
-        <Route path= "/shop" component = {Form}/>        
-             </Switch>"
-                                        
-              </header>
-                                    
-                                                        
-              </div>     
-                                                 
-                         );
-                               
-                         };
-                                                    
-
-       
-
-             export default Apps   
-                                                 
+  import App from './App.css'
+ import React, {useState, useEffect} from "react"
+  import {BrowserRouter, Link, Route, Switch} from "react-router-dom";
+  import * as Yup from "yup";
+   import axios from "axios"
+  import Nav from  "./nav"     
+  import Form from "./Form"
                   
 
                                                 
 
                              
 
+    const Apps = () => {
                          
-                                   
+                        
+                        
+                         
+                     //JSX
+                           return (
+                         
+                          
+                               <div className="APP">
+                                <Nav/> 
+                                   <header className= "Heads">
+                                 <Switch>
+                                                                            
+                            
+                                 <Route path= "/shop" component = {Form}/>        
+                                      </Switch>"
+                                                                 
+                                       </header>
+                                                             
+                                                                                 
+                                       </div>     
+                                                                          
+                                                  );
+                                                        
+                                                  };
+                                                                             
+                         
+                                
+                         
+       export default Apps   
+                                                                       
                              
          
          
