a62b6d0558d943ff5915f0fe44696b9db466af36
Josh Levan <35366199+joshualevan@users.noreply.github.com>
2021-05-21 21:22:58 -0400

    pass test 2

--- cypress/integration/examples/sample_spec.js
+++ cypress/integration/examples/sample_spec.js
@@ -1,1 +1,16 @@
-describe('Can add text to the box')
+describe('Can add text to the box', () => {
+    it('can receive text input', () => {
+        cy.visit("localhost:3000/pizza");
+        cy.get("#name-input")
+        .type("Name").should("have.value", "Name");
+    })
+})
+
+describe('Can select multiple toppings', () => {
+    it('can check toppings', () => {
+        cy.visit("localhost:3000/pizza");
+        cy.get('[type="checkbox"]')
+        .check({ force: true })
+        .should('be.checked')
+    })
+})
