3c19f2041aa3534ea9e8f990ad5b6c164525f54e
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-07-18 22:53:27 -0500

    imported browserrouter as router and wrapped app inside it

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
+import Header from './components/Header.js'
+import { Navbar, NavbarBrand } from 'reactstrap'
+import {Link, Route, Switch} from 'react-router-dom'
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div>
+      <header>
+          <div>site name</div>
+        <Navbar color="light">
+          <NavbarBrand>Lambda eats</NavbarBrand>
+          
+        </Navbar>
+
+      </header>
+        <Header />
+    </div>
   );
 };
 export default App;
