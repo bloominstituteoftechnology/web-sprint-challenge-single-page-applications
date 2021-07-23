63f62997507987c6805ac09cd0dfb3ad3c9ff4cd
Rick <rmansfield@realpeoplesratings.com>
2021-07-23 14:07:46 -0500

    refactor formSchema.js

--- src/validations/formSchema.js
+++ src/validations/formSchema.js
@@ -6,5 +6,14 @@ export default yup.object().shape({
         .string()
         .required("Username is requires")
         .min(2, "name must be at least 2 characters"),
-
+    special: yup
+    .string()
+    .max(100, 'Shorten your message to 100 chars'),
+    size: yup
+    .string()
+    .oneOf(['small', 'medium', 'large', 'x-large'], 'Size is required'),
+    ham: yup.boolean(),
+    olives: yup.boolean(),
+    onions: yup.boolean(),
+    cheese: yup.boolean(),
 })
