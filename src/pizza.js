defd9d7255dcb6a2caf2876262d0e2698dbba766
Rick <rmansfield@realpeoplesratings.com>
2021-07-23 08:26:56 -0500

    add name input to form

--- src/pizza.js
+++ src/pizza.js
@@ -3,7 +3,14 @@ import { Link } from "react-router-dom";
 import { useHistory } from "react-router-dom";
 
 export default function OrderPizza(props) {
-    const { values, submit, change, disabled, errors } = props;
+    const { values, submit, change, disabled, errors, update } = props;
+
+
+    const onChange = evt => {
+        const {name, value} = evt.target
+        update(name, value)//put a debugger on the parent App.js updateForm and verify working. 
+      }
+
     const onSubmit = (evt) => {
         evt.preventDefault();
         submit();
@@ -12,8 +19,18 @@ export default function OrderPizza(props) {
         <form className="form container" id="pizza-form" onSubmit={onSubmit}>
             <div className="form-group submit">
                 <div>
-                    <h2>Add a User</h2>
+                    <h2>Add a User and Pizza </h2>
                 </div>
+                <label>username
+                    <input
+                    name='username'
+                    type='text'
+                    value={onChange}
+                    placeholder='type your unsername'
+                    maxLength='30'
+                    id='name-input'
+                    />
+                </label>
 
                 <button disabled={disabled}>submit</button>
 
