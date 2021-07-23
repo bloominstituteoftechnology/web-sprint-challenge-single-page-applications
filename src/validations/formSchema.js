d09376b1a2c925a5db1eb4f938ef5f2976a9e0fd
Rick <rmansfield@realpeoplesratings.com>
2021-07-23 12:20:31 -0500

    Refactor file names

--- src/validations/formSchema.js
+++ src/validations/formSchema.js
@@ -6,17 +6,5 @@ export default yup.object().shape({
         .string()
         .required("Username is requires")
         .min(2, "name must be at least 2 characters"),
-    // email: yup
-    //     .string()
-    //     .email('Must be a valid email')
-    //     .required('Email is required'),
-    // password: yup
-    //     .string()
-    //     .min(8).max(16)
-    //     .required('Must provide password'),
-    // terms: yup
-    //     .boolean()
-    //     .oneOf([true], 'must agree to terms' )
-    //     // .required('Must agree to terms')
 
 })
