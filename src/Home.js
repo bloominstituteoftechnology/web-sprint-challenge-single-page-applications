13a0f14148e2fd03080dd53ed530eac8662629f8
nicole pizzano <pizzano.nicole@gmail.com>
2021-08-14 19:27:52 -0400

    added routes

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,22 @@
-import React from "react";
+import React from 'react';
 import "./index.css";
+import {Link} from "react-router-dom";
+import {Pizza} from "./Pizza";
 
 
 
 
 export default function Home() {
     return(
-    <div className="Home">
-    <h1>Lambda Eats</h1>
-    <h2> Voted Best Pizza in Town</h2>
-    <p>Online ordering now available!</p>
-    
-      </div>
-    )
-   
-}
+        <div id="pizzaorder">
+        <h1>Lambda Eats</h1>
+        <h2> Voted Best Pizza in Town</h2>
+     <button id="order-pizza" onClick={() => {
+         window.location.href = "/pizza/1";
+       
+         }}>Get Pizza</button>
+
+        <p>Online ordering now available!</p>
+        
+          </div>
+    )}
