654c2563dc6af1ba77e29855d809a0fdd097cbdf
vasilii-garanin <vasilii_garanin@laptevfamily.com>
2021-08-20 09:51:34 -0500

    For codegrade set up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 
-const App = () => {
-  return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
-  );
+export default const App = () =>
+{
+    return (
+        <>
+            <h1>Lambda Eats</h1>
+            <p>You can remove this code and create your own header</p>
+        </>
+    );
 };
-export default App;
+
