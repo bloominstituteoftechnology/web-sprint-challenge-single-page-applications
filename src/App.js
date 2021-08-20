8fcada4652f0e9e009513ff722127fe50edb2a4e
Anastasia Lapteva <anastasia_laptev@hotmail.com>
2021-08-20 12:29:03 -0700

    built app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,106 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Route, Link, Switch } from 'react-router-dom';
+import Confirmation from "./Confirmation";
+import Order from "./Order";
+import Home from "./Home";
+import schema from '../validation/formSchema';
+import * as yup from 'yup';
 
-const App = () => {
-  return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
-  );
+const initialFormValues = {
+    customerName: '',
+    pizzaSize: '',
+    pineapple: false,
+    sausage: false,
+    cilantro: false,
+    onion: false,
+    chicken: false,
+    tomato: false,
+    olives: false,
+    special: ''
 };
-export default App;
+
+const initialFormErrors = {
+    customerName: '',
+    pizzaSize: ''
+};
+
+const initialDisabled = true;
+
+export default function App()
+{
+    const [formValues, setFormValues] = useState(initialFormValues); // object
+    const [formErrors, setFormErrors] = useState(initialFormErrors); // object
+    const [confirmation, setConfirmation] = useState(initialFormValues);
+    const [disabled, setDisabled] = useState(initialDisabled);       // boolean
+
+    const setNewOrder = (newOrder) =>
+    {
+        setConfirmation(newOrder);
+        setFormValues(initialFormValues);
+    };
+
+    const validate = (name, value) =>
+    {
+        yup.reach(schema, name)
+            .validate(value)
+            .then(() => setFormErrors({ ...formErrors, [name]: '' }))
+            .catch(err => setFormErrors({ ...formErrors, [name]: err.errors[0] }));
+    };
+
+    const inputChange = (name, value) =>
+    {
+        validate(name, value);
+        setFormValues({
+            ...formValues,
+            [name]: value
+        });
+    };
+
+    const formSubmit = () =>
+    {
+        const newOrder = {
+            customerName: formValues.customerName.trim(),
+            pizzaSize: formValues.pizzaSize,
+            toppings: ['pineapple', 'sausage', 'cilantro', 'onion', 'chicken', 'tomato', 'olives'].filter(topping => !!formValues[topping]),
+            special: formValues.special.trim()
+        };
+
+        setNewOrder(newOrder);
+    };
+
+    useEffect(() =>
+    {
+        schema.isValid(formValues).then(valid => setDisabled(!valid));
+    }, [formValues]);
+
+
+    return (
+        <div className='container'>
+            <nav>
+                <h1 className='pizza-header'>Lambda Eats</h1>
+                <div className='nav-links'>
+                    <Link to="/">Home</Link>
+                    <Link to="/pizza">Order</Link>
+                </div>
+            </nav>
+
+            <Switch>
+                <Route path="/pizza">
+                    <Order
+                        values={formValues}
+                        change={inputChange}
+                        submit={formSubmit}
+                        disabled={disabled}
+                        errors={formErrors}
+                    />
+                </Route>
+                <Route path="/pizza/confirmation">
+                    <Confirmation details={confirmation} />
+                </Route>
+                <Route path="/">
+                    <Home />
+                </Route>
+            </Switch>
+        </div>
+    );
+};
