e8ff7cd208e3ae081c3bc9c416c381cd20a39610
Mallory Shea <malloryshea@Mallorys-MacBook-Air.local>
2021-11-12 15:37:43 -0600

    form validation passing 8 tests final push

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,108 +1,177 @@
-import { useState } from "react";
-
-const Form = () => {
-  const [inputs, setInputs] = useState({});
-
-  const handleChange = (event) => {
-    const name = event.target.name;
-    const value = event.target.value;
-    setInputs((values) => ({ ...values, [name]: value }));
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    if (inputs.name.length < 2) {
-      alert("name must be at least 2 characters");
-    }
-  };
-  return (
-    <form id="pizza-form" onSubmit={handleSubmit}>
-      <label htmlFor="Name">
-        Enter your name:
-        <input
-          id="name-input"
-          type="text"
-          name="name"
-          value={inputs.name || ""}
-          onChange={handleChange}
-        />
-      </label>
-      <br />
-
-      <label htmlFor="Toppings">
-        <input
-          type="checkbox"
-          name="cheese"
-          value={inputs.cheese || "cheese"}
-          onChange={handleChange}
-        />{" "}
-        cheese:
-      </label>
-      <br />
-      <label htmlFor="Toppings">
-        <input
-          type="checkbox"
-          name="pineapple"
-          value={inputs.pineapple || "pineapple"}
-          onChange={handleChange}
-        />
-        pineapple:
-      </label>
-      <br />
-      <label htmlFor="Toppings">
-        <input
-          type="checkbox"
-          name="pepperoni"
-          value={inputs.pepperoni || "pepperoni"}
-          onChange={handleChange}
-        />
-        pepperoni:
-      </label>
-      <br />
-      <label htmlFor="Toppings">
-        <input
-          type="checkbox"
-          name="olives"
-          value={inputs.olives || "olives"}
-          onChange={handleChange}
-        />{" "}
-        olives:
-      </label>
-      <br />
+import React, { useState, useEffect } from "react";
+import * as yup from 'yup';
+import axios from 'axios';
 
 
-      <label htmlFor="Size">
-        <select
-          id="size-dropdown"
-          className="Size"
-          name="Size"
-          onChange={handleChange}
-          value={inputs.Size || "Size"}
-        >
-        <option value="SMALL">SMALL</option>
-        <option value="MEDIUM">MEDIUM</option>
-        <option value="LARGE">LARGE</option>
-        
-        </select>
-      </label>
-      <br />
 
-      <label htmlFor="special-text">Special Instructions:</label>
-      <textarea
-        id="special-text"
-        name="specialText"
-        value={inputs.specialText || ""}
-        onChange={handleChange}
-        
-      ></textarea>
-      <br />
+const yupForm = yup.object().shape({
+    name: yup.string().required('name is required').min(2, 'name must be at least 2 characters'),
+    size: yup.string().oneOf(['Small', 'Medium', 'Large']),
+    special: yup.string(),
+    cheese: yup.boolean(),
+    pineapples: yup.boolean(),
+    pepperoni: yup.boolean(),
+    olives: yup.boolean(),
+    instructions: yup.string(),
+})
 
-      {/* <input id="order-button" type="submit" /> */}
-      <button id="order-button" type="submit">
-        Order
-      </button>
-    </form>
-  );
-};
+const defaultVal = {
+    name: "",
+    sauce: "",
+    size: "",
+    special: "",
+    cheese: false,
+    pineapples: false,
+    pepperoni: false,
+    olives: false,
+    instructions: "",
+}
+
+function Form() {
+
+    const [isValid, setIsValid] = useState(true);
+
+    const [form, setForm] = useState(defaultVal);
+
+    const [errorState, setError] = useState({
+        name: "",
+        size: "",
+        special: "",
+        cheese: "",
+        pineapples: "",
+        pepperoni: "",
+        instructions: "",
+    })
+
+    useEffect(() => {
+        yupForm.isValid(form)
+            .then(valid => {
+            });
+    }, [form]);
+
+
+    const validate = (e) => {
+        yup.reach(yupForm, e.target.name)
+            .validate(e.target.value)
+            .then(valid => {
+                setError({
+                    ...errorState,
+                    [e.target.name]: ""
+                })
+
+            })
+            .catch(error => {
+                console.log(error.errors)
+                setError({
+                    ...errorState,
+                    [e.target.name]: error.errors[0]
+                })
+            })
+    };
+
+    const inputChange = e => {
+        e.persist();
+        validate(e)
+
+        let value = e.target.type === "checkbox" ? e.target.checked : e.target.value
+        setForm({ ...form, [e.target.name]: value });
+    };
+
+    const formSubmit = (e) => {
+        e.preventDefault();
+        axios.post(`https://reqres.in/api/orders`, form)
+            .then(res => { console.log('RES', res) })
+            .catch(err => console.log(err.response));
+        setForm(defaultVal)
+    };
+
+    return (
+        <div>
+            <h1>Create your Pizza!!</h1>
+
+            <form onSubmit={formSubmit} id="pizza-form">
+                <label 
+                htmlFor="name">Your Name: 
+                </label>
+                <input
+                    id="name-input"
+                    name="name"
+                    type="text"
+                    placeholder="Name"
+                    value={form.name}
+                    onChange={inputChange}
+                />
+                
+                {errorState.name.length > 1 ? <p className="error">{errorState.name}</p> : null}
+
+                <p>
+                <label htmlFor="toppings">Pick your Toppings!</label>
+                </p>
+                <p>
+                    <input
+                        id="toppings"
+                        type="checkbox"
+                        checked={form.cheese}
+                        onChange={inputChange}
+                        name="cheese"
+                    />cheese
+                    <input
+                        id="toppings"
+                        type="checkbox"
+                        checked={form.pineapples}
+                        onChange={inputChange}
+                        name="pineapples"
+                    />pineapples
+                    <input
+                        id="toppings"
+                        type="checkbox"
+                        checked={form.pepperoni}
+                        onChange={inputChange}
+                        name="pepperoni"
+                    />pepperoni
+                    <input 
+                        id="toppings"
+                        type="checkbox"
+                        checked={form.olives}
+                        onChange={inputChange}
+                        name="olives"
+                    />olives
+                    
+                </p>
+                <p><label htmlFor="size-dropdown">Pick your size!! </label>
+                    <select 
+                    id="size-dropdown" 
+                    name="size" 
+                    value={form.size} 
+                    onChange={inputChange}>
+                        <option value="Small">Small</option>
+                        <option value="Medium">Medium</option>
+                        <option value="Large">Large</option>
+                    </select>
+                </p>
+               
+                <p><label htmlFor="instructions">Special Instructions: </label>
+                    <textarea
+                        name="instructions"
+                        id="special-text"
+                        value={form.instructions}
+                        onChange={inputChange}
+                    /></p>
+
+                <button 
+                name="order-button" 
+                id="order-button" 
+                type="submit">
+                    Place Order
+                </button>
+
+            </form>
+        </div>
+    );
+
+
+}
+
 
 export default Form;
+
--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -1,5 +1,5 @@
 const Home = () => {
-    return <h1>Home</h1>
+    return <h1>Click form above and start building your pizza!!</h1>
   }
   
   export default Home;
