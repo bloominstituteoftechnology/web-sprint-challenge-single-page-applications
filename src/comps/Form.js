d52f7823b197a4fbc4c7a6095fffda34142dd6fe
Ibrahim <Ibrahimagbor1@gmail.com>
2023-08-22 16:58:43 -0400

    done

--- src/comps/Form.js
+++ src/comps/Form.js
@@ -1,5 +1,5 @@
-import React, {useState, useEffect} from 'react'
-import axios from 'axios'
+import React from 'react'
+
 
 
 function Form(props) {
--- src/comps/formSchema.js
+++ src/comps/formSchema.js
@@ -3,10 +3,10 @@ import * as yup from "yup"
 const formschema = yup.object().shape({
     name: yup.string().required("name must be at least 2 characters").min(2, "name must be at least 2 characters"),
     size: yup.string().oneOf(["1","2","3","4"], 'you must chose your size'),
-    topping1: yup.boolean,
-    topping2: yup.boolean,
-    topping3: yup.boolean,
-    topping4: yup.boolean,
+    topping1: yup.boolean(),
+    topping2: yup.boolean(),
+    topping3: yup.boolean(),
+    topping4: yup.boolean(),
     specialText: yup.string()
   
    
