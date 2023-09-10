a4187f5ff96ba1b88ea673ed01920e2b6a45e57a
mrzexii <126282962+mrzexii@users.noreply.github.com>
2023-09-10 11:11:34 -0700

    Update index.js

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,1 +1,18 @@
 
+import React from 'react'
+import { useNavigate } from 'react-router-dom';
+
+export default function Index() {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate('/pizza');
+  }
+  return (
+    <div className="main-body">
+        <div className="pizza-body">
+          <p>Your favorite food, delivered while coding</p>
+          <button id="order-pizza" onClick={() => handleClick()}>Pizza?</button>
+        </div>
+    </div>
+  )
+}
